dd3b9da6daaac98ad0d06904a0f4374c2bc8b20e
AdriaMelero1 <adriamelero1@gmail.com>
2023-10-10 16:57:19 +0200

    Solo impares

--- UD4/TablasMultiplicarArrays/script.js
+++ UD4/TablasMultiplicarArrays/script.js
@@ -1,7 +1,7 @@
 
 
 //Primer declaram l'array amb 10 posicions
-let tabla = new Array(10);
+let tabla = new Array(11);
 
 
 //Dins cada posicio del array declaram un nou array de 10 posicions
@@ -12,11 +12,14 @@ for(let i = 0; i < tabla.length; i++) {
 
 //Recorrem cada posicio del array i l'array dins de cada posicio
 for(let fila = 0; fila < tabla.length; fila++){
+    console.log("Tabla del ", fila);
     for(let col = 0; col < tabla[fila].length; col++){
         //Asignam a cada posicio el valor del producte de l'index de l'array principal i el subarray
         tabla[fila][col] = fila * col;
-        //Imprimim les tables de multiplicar
-        console.log(fila," * ", col, " = ", tabla[fila][col]);
+        //Imprimim les tables de multiplicar si el resultat es impar
+        if(tabla[fila][col] % 2 != 0){
+            console.log(fila," * ", col, " = ", tabla[fila][col]);
+        }
     }
 }
 
