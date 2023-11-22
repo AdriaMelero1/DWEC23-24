cb0c3046487ab72bcc7e199c842376af2dc8e67f
AdriaMelero1 <adriamelero1@gmail.com>
2023-11-22 21:19:42 +0100

    Form validation working, cinema with video on screen and hanging man layout started

--- Forms Validation/script.js
+++ Forms Validation/script.js
@@ -6,21 +6,51 @@ const password2 = document.getElementById('password2');
 
 //FUNCTIONS
 
-function isMandatory(input) {
+/* function isMandatory(input) {
     if(input.value.trim( ) === ''){
         displayError(input, 'It\'s Mandatory')
     } else {
         displayCorrect(input);
     }
+} */
+
+function isMandatory(inputArray) {
+
+    inputArray.forEach((input) => {
+        if(input.value.trim() === ''){
+            displayError(input, `${takeInputName(input)} It s Mandatory`);
+        } else {
+            displayCorrect(input);
+        }
+    });
 }
 
 function checkLength(input, min, max) {
     if(input.value.length < min){
-        displayError(input, 'Passwords must have at least ' + min + ' characters');    
+        displayError(input, `${takeInputName(input)} must have at least ${min} characters`);    
     } else if(input.value.length > max){
-        displayError(input, 'Passwords must have less than ' + max + ' characters');    
+        displayError(input, `${takeInputName(input)} must have less than ${max} characters`);    
     } else {
-        displayCorrect();
+        displayCorrect(input);
+    }
+}
+
+function isEmailValid(input){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if(re.test(input.value.trim())){
+        displayCorrect(input);
+    } else {
+        let message = takeInputName(input) + " format not valid";
+        displayError(input, message);
+    }
+}
+
+function checkPasswordsAreEqual(input1, input2){
+
+    if(input1.value != input2.value){
+        let message = takeInputName(input2) + " Must be equal to " + takeInputName(input1);
+        displayError(input2, message)
     }
 }
 
@@ -47,9 +77,16 @@ function takeInputName(input){
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    isMandatory(username);
-    isMandatory(email);
-    isMandatory(password);
-    isMandatory(password2);
+    
+    isMandatory([username, email, password, password2]);
+    
     checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    
+    isEmailValid(email);
+    
+    checkPasswordsAreEqual(password, password2);
 });
+
+
+
